008e68fe3bf949fe8a24fe86450fa6d1e308c33a
Edwin Fullard <edwin.fullard@entelect.nl>
2020-04-01 10:49:23 +0200

    Added checkboxes for the names selection and refactored some code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,14 +34,17 @@
             this.JokeType = new System.Windows.Forms.Label();
             this.Timer = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.NamesCheckList = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(68, 368);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.button1.Location = new System.Drawing.Point(269, 305);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(205, 143);
+            this.button1.Size = new System.Drawing.Size(443, 58);
             this.button1.TabIndex = 0;
             this.button1.Text = "Randomize";
             this.button1.UseVisualStyleBackColor = true;
@@ -50,30 +53,36 @@
             // UserName
             // 
             this.UserName.AutoSize = true;
-            this.UserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UserName.Location = new System.Drawing.Point(49, 94);
+            this.UserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.UserName.Location = new System.Drawing.Point(269, 95);
+            this.UserName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.UserName.Name = "UserName";
-            this.UserName.Size = new System.Drawing.Size(315, 113);
+            this.UserName.Size = new System.Drawing.Size(252, 91);
             this.UserName.TabIndex = 2;
             this.UserName.Text = "Name";
             // 
             // JokeType
             // 
             this.JokeType.AutoSize = true;
-            this.JokeType.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.JokeType.Location = new System.Drawing.Point(49, 225);
+            this.JokeType.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.JokeType.Location = new System.Drawing.Point(269, 186);
+            this.JokeType.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.JokeType.Name = "JokeType";
-            this.JokeType.Size = new System.Drawing.Size(510, 113);
+            this.JokeType.Size = new System.Drawing.Size(405, 91);
             this.JokeType.TabIndex = 3;
             this.JokeType.Text = "Joke Type";
             // 
             // Timer
             // 
             this.Timer.AutoSize = true;
-            this.Timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Timer.Location = new System.Drawing.Point(326, 381);
+            this.Timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.Timer.Location = new System.Drawing.Point(269, 387);
+            this.Timer.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Timer.Name = "Timer";
-            this.Timer.Size = new System.Drawing.Size(304, 113);
+            this.Timer.Size = new System.Drawing.Size(243, 91);
             this.Timer.TabIndex = 4;
             this.Timer.Text = "Timer";
             // 
@@ -81,20 +90,30 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // NamesCheckList
+            // 
+            this.NamesCheckList.FormattingEnabled = true;
+            this.NamesCheckList.Location = new System.Drawing.Point(26, 108);
+            this.NamesCheckList.Name = "NamesCheckList";
+            this.NamesCheckList.Size = new System.Drawing.Size(203, 400);
+            this.NamesCheckList.TabIndex = 5;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1028, 623);
+            this.ClientSize = new System.Drawing.Size(899, 584);
+            this.Controls.Add(this.NamesCheckList);
             this.Controls.Add(this.Timer);
             this.Controls.Add(this.JokeType);
             this.Controls.Add(this.UserName);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(17, 60, 17, 18);
             this.Text = "Stand-up Meeting";
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
@@ -104,6 +123,7 @@
         private System.Windows.Forms.Label JokeType;
         private System.Windows.Forms.Label Timer;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckedListBox NamesCheckList;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,110 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MetroFramework.Forms;
 
 namespace DiceRandomizer
 {
-    public partial class Form1 : MetroFramework.Forms.MetroForm
+    public partial class Form1 : MetroForm
     {
-        private int timer_count = 0;
+        private int _timerCount = 0;
+        private List<string> _names = new List<string>();
+        private List<string> _jokes = new List<string>();
+        private Random _randomizer = new Random();
 
         public Form1()
         {
             InitializeComponent();
 
             Timer.Text = "";
+
             CheckFiles();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            timer_count = 11;
-
-            timer1.Start();
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            if (timer_count > 0)
-            {
-                timer_count--;
-                Thread.Sleep(500);
-                Timer.Text = timer_count.ToString();
-                Randomize();
-            }
-            else
-            { 
-                timer1.Stop();
-                Timer.Text = "Tag, you're it!";
-            }
-        }
-
-        public void CheckFiles()
+        private void CheckFiles()
         {
             if (File.Exists("names.txt"))
             {
-                if (File.ReadAllLines("names.txt").Length == 0)
+                try
                 {
-                    MessageBox.Show("Add names below each other in the names.txt file. The application will now close.", "No names found!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    Environment.Exit(0);
+                    foreach (var name in File.ReadAllLines("names.txt"))
+                    {
+                        NamesCheckList.Items.Add(name, true);
+                    }
+                }
+                catch (Exception)
+                {
+                    ShowErrorAndExit(
+                        "Add names below each other in the names.txt file. The application will now close.",
+                        "No names found!");
                 }
             }
             else
             {
                 File.Create("names.txt").Dispose();
-                MessageBox.Show("File names.txt created. Add names below each other in the names.txt file. The application will now close.", "Add names to the file!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Environment.Exit(0);
+                ShowErrorAndExit(
+                    "File names.txt created. Add names below each other in the names.txt file. The application will now close.",
+                    "Add names to the file!");
             }
 
             if (File.Exists("jokes.txt"))
             {
-                if (File.ReadAllLines("jokes.txt").Length == 0)
+                try
                 {
-                    MessageBox.Show("Add jokes below each other in the jokes.txt file. The application will now close.", "No jokes found!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    Environment.Exit(0);
+                    _jokes = File.ReadAllLines("jokes.txt").ToList();
+                }
+                catch (Exception)
+                {
+                    ShowErrorAndExit(
+                        "Add jokes below each other in the jokes.txt file. The application will now close.",
+                        "No jokes found!");
                 }
             }
             else
             {
                 File.Create("jokes.txt").Dispose();
-                MessageBox.Show("File jokes.txt created. Add jokes below each other in the jokes.txt file. The application will now close.", "Add jokes to the file!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Environment.Exit(0);
+                ShowErrorAndExit(
+                    "File jokes.txt created. Add jokes below each other in the jokes.txt file. The application will now close.",
+                    "Add jokes to the file!");
+            }
+        }
+
+        private static void ShowErrorAndExit(string message, string title)
+        {
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Environment.Exit(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _timerCount = 30;
+
+            _names = NamesCheckList.CheckedItems.Cast<string>().ToList();
+
+            Timer.Text = "";
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (_timerCount > 0)
+            {
+                _timerCount--;
+                Thread.Sleep(100);
+                Randomize();
+            }
+            else
+            {
+                timer1.Stop();
+                Timer.Text = @"Tag, you're it!";
             }
         }
 
         public void Randomize()
         {
-            try
-            {
-                string[] allLines1 = File.ReadAllLines("names.txt");
-                Random rnd1 = new Random();
-                UserName.Text = (allLines1[rnd1.Next(allLines1.Length)]);
-            }
-            catch(IndexOutOfRangeException)
-            {
-                MessageBox.Show("Add names below each other in the names.txt file. The application will now close.", "No names found!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Environment.Exit(0);
-            }
-
-            try
-            {
-                string[] allLines2 = File.ReadAllLines("jokes.txt");
-                Random rnd2 = new Random();
-                JokeType.Text = (allLines2[rnd2.Next(allLines2.Length)]);
-            }
-            catch(IndexOutOfRangeException)
-            {
-                MessageBox.Show("Add jokes below each other in the jokes.txt file. The application will now close.", "No jokes found!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Environment.Exit(0);
-            }
+            UserName.Text = _names[_randomizer.Next(_names.Count)];
+            JokeType.Text = _jokes[_randomizer.Next(_jokes.Count)];
         }
     }
 }
